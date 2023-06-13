434972ed0cd5d5ce12a7ecb51a95860e8ac2620e
dajcs <Attila.Nemet@gmail.com>
2023-06-13 11:31:16 +0200

    static tf: world - Attila - leo03/resource

--- pose_client.py
+++ pose_client.py
@@ -22,8 +22,9 @@ ros2 launch vrpn_client_ros sample.launch.py
 ros2 launch optitrack_tf optitrack_tf.launch.py
 
 # tf from world \ Attila --> leo09/base_footprint \ leo09/base_link
-ros2 run tf2_ros static_transform_publisher  0 0 -0.05  0 0 0  Attila leo09/base_link
+ros2 run tf2_ros static_transform_publisher  0 0 -0.05  0 0 0  Attila leo09/base_footprint
 ros2 run tf2_ros static_transform_publisher  -0.12 0 -0.40  0 0 0  Attila leo02/base_footprint
+ros2 run tf2_ros static_transform_publisher  -0.12 0 -0.40  0 0 0  Attila leo03/base_footprint
 
 # add image to rviz
 -> image -> /leo02/camera/image_raw
@@ -109,73 +110,23 @@ ism_student_ros2@ll106:~$ ros2 run my_turtlesim pose_client
         Orientation:
         geometry_msgs.msg.Quaternion(x=0.13628806173801422, y=0.014444611966609955, z=-0.5753024220466614, w=0.8063771724700928)
 
+        
 ism_student_ros2@ll106:~$ ros2 run tf2_ros tf2_echo world Attila
-[INFO] [1686126520.711680481] [tf2_echo]: Waiting for transform world ->  Attila: Invalid frame ID "world" passed to canTransform argument target_frame - frame does not exist
-At time 1686126521.670799271
-- Translation: [1.891, -4.702, 0.650]
-- Rotation: in Quaternion [0.214, -0.106, -0.553, 0.798]
-At time 1686126521.800705911
-- Translation: [1.883, -4.717, 0.653]
-- Rotation: in Quaternion [0.135, 0.013, -0.575, 0.806]
-At time 1686126522.920637227
-- Translation: [1.890, -4.702, 0.651]
-- Rotation: in Quaternion [0.203, -0.093, -0.549, 0.805]
-At time 1686126524.680494213
-- Translation: [1.883, -4.717, 0.653]
-- Rotation: in Quaternion [0.137, 0.013, -0.578, 0.804]
-At time 1686126525.680545003
-- Translation: [1.914, -4.675, 0.637]
-- Rotation: in Quaternion [0.481, 0.120, -0.324, 0.806]
-At time 1686126526.650872360
-- Translation: [1.870, -4.729, 0.656]
-- Rotation: in Quaternion [0.001, -0.020, -0.679, 0.733]
-^C[INFO] [1686126526.771579932] [rclcpp]: signal_handler(signal_value=2)
-ism_student_ros2@ll106:~$ ros2 run tf2_ros tf2_echo Attila leo02/resource
-[INFO] [1686126544.578260951] [tf2_echo]: Waiting for transform Attila ->  leo02/resource: Invalid frame ID "Attila" passed to canTransform argument target_frame - frame does not exist
+At time 1686642331.216446501
+- Translation: [3.724, -1.577, 0.575]
+- Rotation: in Quaternion [0.023, -0.058, -0.768, 0.638]
+
+
+ism_student_ros2@ll106:~$ ros2 run tf2_ros tf2_echo Attila leo03/resource
 At time 0.0
 - Translation: [0.368, 0.000, -0.328]
 - Rotation: in Quaternion [0.000, 0.005, 0.000, 1.000]
-At time 0.0
-- Translation: [0.368, 0.000, -0.328]
-- Rotation: in Quaternion [0.000, 0.005, 0.000, 1.000]
-^C[INFO] [1686126548.500518919] [rclcpp]: signal_handler(signal_value=2)
 
 
-ism_student_ros2@ll106:~$ ros2 run tf2_ros tf2_echo world leo02/resource
-[INFO] [1686126603.329063190] [tf2_echo]: Waiting for transform world ->  leo02/resource: Invalid frame ID "world" passed to canTransform argument target_frame - frame does not exist
-At time 1686126605.281063572
-- Translation: [2.161, -5.033, 0.395]
-- Rotation: in Quaternion [0.266, -0.222, -0.618, 0.706]
-At time 1686126606.201115461
-- Translation: [2.038, -4.984, 0.268]
-- Rotation: in Quaternion [0.140, 0.020, -0.589, 0.795]
-At time 1686126607.300996200
-- Translation: [2.096, -5.063, 0.372]
-- Rotation: in Quaternion [0.210, -0.198, -0.664, 0.689]
-At time 1686126608.290956314
-- Translation: [2.050, -4.982, 0.272]
-- Rotation: in Quaternion [0.138, 0.016, -0.578, 0.804]
-At time 1686126609.281270339
-- Translation: [1.750, -5.121, 0.360]
-- Rotation: in Quaternion [-0.102, 0.076, -0.728, 0.674]
-At time 1686126610.281229215
-- Translation: [2.090, -5.064, 0.368]
-- Rotation: in Quaternion [0.203, -0.191, -0.667, 0.691]
-At time 1686126611.291344046
-- Translation: [2.050, -4.982, 0.272]
-- Rotation: in Quaternion [0.138, 0.017, -0.577, 0.805]
-At time 1686126612.301247348
-- Translation: [1.905, -5.105, 0.338]
-- Rotation: in Quaternion [0.002, -0.015, -0.680, 0.733]
-At time 1686126613.291106344
-- Translation: [1.906, -5.105, 0.338]
-- Rotation: in Quaternion [0.003, -0.016, -0.680, 0.733]
-At time 1686126614.301197952
-- Translation: [2.051, -4.981, 0.272]
-- Rotation: in Quaternion [0.139, 0.017, -0.576, 0.805]
-At time 1686126615.281139383
-- Translation: [2.051, -4.982, 0.272]
-- Rotation: in Quaternion [0.138, 0.017, -0.576, 0.806]
+ism_student_ros2@ll106:~$ ros2 run tf2_ros tf2_echo world leo03/resource
+At time 1686642475.847782934
+- Translation: [3.683, -1.954, 0.262]
+- Rotation: in Quaternion [0.021, -0.047, -0.769, 0.637]
 
 
 
